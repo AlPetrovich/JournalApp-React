7253a82979e8fdb7446e6b61b32fce9f722e8550
alpetrovich <alexispetrovich11@gmail.com>
2022-08-16 15:16:11 -0300

    navbar and sidebar MATERIAL UI

--- journal-app/src/journal/pages/JournalPage.jsx
+++ journal-app/src/journal/pages/JournalPage.jsx
@@ -1,11 +1,18 @@
 import { MailOutline } from "@mui/icons-material";
 import { Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
+
+
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">JournalPage</Typography>
-      <MailOutline />
-    </>
+    <JournalLayout>
+
+      <Typography>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam magni aliquid illum? Quae ipsa a rem repellendus exercitationem neque? Nostrum quibusdam a aliquam illum recusandae reprehenderit. Quia perspiciatis mollitia molestias?</Typography>
+      
+      {/* NothinSelected */}
+      {/* NoteView */}
+      
+    </JournalLayout>
   )
 }
