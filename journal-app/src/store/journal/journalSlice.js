c50a694b6d380c9b9986f36f3721ec6c39c58f76
alpetrovich <alexispetrovich11@gmail.com>
2022-08-22 16:16:05 -0300

    redux-slice crud journal

---
+++ journal-app/src/store/journal/journalSlice.js
@@ -0,0 +1,48 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const journalSlice = createSlice({
+   name: 'journal',
+   initialState: { 
+      isSaving: true,
+      messageSave: '',
+      notes: [],
+      activeNote: null,
+    //   activeNote: {
+    //         id: 'ABC1234',
+    //         title: '',
+    //         body: '',
+    //         date: 1234567,
+    //         imageUrls: [], //https://foto1.jpg, https://foto2.jpg 
+    //   }
+   },
+   reducers: {
+       addNewEmptyNote: (state, action) => {
+
+       },
+         setActiveNote: (state, action) => {
+
+        },
+        setNotes: (state, action) => {
+
+        },
+        setIsSaving: (state) => {
+
+        },
+        updateNote: (state, action) => {
+
+        },
+        deleteNoteById: (state, action) => {
+
+        }
+    }
+});
+
+// acciones para caer dentro de nuestro reducer
+export const {
+    addNewEmptyNote,
+    setActiveNote,
+    setNotes,
+    setIsSaving,
+    updateNote,
+    deleteNoteById,
+} = journalSlice.actions;
